7e9898008b16785995db7b79943a39c4746f691d
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2019-12-11 10:52:46 -0500

    05 - Get The Player Controller with C++

--- BattleStarship/Source/BattleStarship/Private/StarshipAIController.cpp
+++ BattleStarship/Source/BattleStarship/Private/StarshipAIController.cpp
@@ -2,24 +2,35 @@
 
 #include "Starship.h"
 #include "StarshipAIController.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
 
 void AStarshipAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto Enemy = GetEnemyStarship();
+	auto Player = GetPlayerStarship();
 
-	if (!Enemy)
+	if (!Player)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("The Enemy Starships are NOT possessed!"));
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller cannot find the Player!"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Controller is possessed by: %s!"), *(Enemy->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller found player: %s!"), *(Player->GetName()));
 	}
+
 }
 	AStarship* AStarshipAIController::GetEnemyStarship() const
 	{
 		return Cast<AStarship>(GetPawn());
+	}
+
+	AStarship* AStarshipAIController::GetPlayerStarship() const
+	{
+		auto PlayerController = GetWorld()->GetFirstPlayerController();
+		if (!PlayerController) { return nullptr; }
+		return Cast<AStarship>(PlayerController);
 	}
 
--- BattleStarship/Source/BattleStarship/Public/StarshipAIController.h
+++ BattleStarship/Source/BattleStarship/Public/StarshipAIController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Engine/World.h"
 #include "Starship.h"
 #include "StarshipAIController.generated.h"
 
@@ -16,7 +17,12 @@ class BATTLESTARSHIP_API AStarshipAIController : public AAIController
 	GENERATED_BODY()
 	
 public: 
+	void virtual BeginPlay() override;
+
+private:
 	AStarship* GetEnemyStarship() const;
 
-	void virtual BeginPlay() override;
+	AStarship* GetPlayerStarship() const;
+
+
 };
